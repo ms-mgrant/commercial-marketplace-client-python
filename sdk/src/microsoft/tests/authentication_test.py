aca753ba1fb7a9bbbb4355a2abbd1fc1c00f8d90
Scott Seely <scseely@microsoft.com>
2020-09-21 16:03:07 -0700

    Got a test to run

--- sdk/src/microsoft/tests/authentication_test.py
+++ sdk/src/microsoft/tests/authentication_test.py
@@ -1,6 +1,9 @@
 
 from azure.identity import ClientSecretCredential
 from azure.identity import CertificateCredential
-from ..microsoft.marketplace.saas import
+from src.microsoft.marketplace.saas import SaaSAPI
+
+def test_true():
+    assert 3 == 3
 
 
