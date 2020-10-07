582811254bff25a2d3c42ec53c49c0b789248c1e
Scott Seely <scseely@microsoft.com>
2020-10-06 18:55:34 -0700

    Got simplified code working

--- sdk/src/microsoft/tests/authentication_test.py
+++ sdk/src/microsoft/tests/authentication_test.py
@@ -1,50 +1,61 @@
-import base64
 import os
+import pathlib
 
-from azure.identity import CertificateCredential
-from cryptography.hazmat.primitives.serialization import pkcs12
+from azure.identity import CertificateCredential, ClientSecretCredential
 
-from src.microsoft.marketplace.token_provider import MarketplaceTokenProvider
+from src.microsoft.marketplace.saas import SaaSAPI
+from src.microsoft.marketplace.saas.models import SubscriptionStatusEnum
 
 
-def test_client_secret_auth():
+def get_client_secret_credential():
     tenant_id = os.environ["AAD_TENANT_ID"]
     client_id = os.environ["AAD_APP_CLIENT_ID"]
     client_secret = os.environ["AAD_APP_CLIENT_SECRET"]
 
-    provider = MarketplaceTokenProvider(
+    cred = ClientSecretCredential(
         tenant_id=tenant_id,
         client_id=client_id,
         client_secret=client_secret
     )
-    token = provider.acquire_token()
-    assert (token['token_type'] == "Bearer")
-    assert (token['expires_in'] == 3599)
-    assert (len(token['access_token']) > 0)
+
+    return cred
+
+
+def test_get_subscriptions():
+
+    saas = SaaSAPI(get_client_secret_credential())
+    subscription_found = False
+    subscriptions = saas.fulfillment_operations.list_subscriptions()
+
+    for sub in subscriptions:
+        if sub.saas_subscription_status == SubscriptionStatusEnum.SUBSCRIBED:
+            subscription_found = True
+
+    assert subscription_found
 
 
 def test_client_certificate_auth():
-    # cred = CertificateCredential
+    current_path = pathlib.Path(__file__).parent.absolute()
     tenant_id = os.environ["AAD_TENANT_ID"]
     client_id = os.environ["AAD_APP_CLIENT_ID"]
-    client_cert = os.environ["AAD_APP_CERT"]
+    certificate_relative_path = pathlib.Path(os.environ["AAD_CERTIFICATE_PATH"])
+    certificate_path = current_path.joinpath(certificate_relative_path)
     cert_secret = os.environ["AAD_APP_CERT_SECRET"]
-    cert_thumbprint = os.environ["AAD_APP_THUMBPRINT"]
-
-    cert_bytes = base64.b64decode(client_cert)
-    password_bytes = bytes(cert_secret, "utf-8")
-    cert_parts = pkcs12.load_key_and_certificates(cert_bytes, password_bytes)
-    private_key = cert_parts[0]
-    pem_file = open('d:/key.pkcs12', 'wb')
-    pem_file.write(cert_bytes)
-    provider = MarketplaceTokenProvider(
+    cred = CertificateCredential(
         tenant_id=tenant_id,
         client_id=client_id,
-        client_secret={"thumbprint": cert_thumbprint, "private_key": private_key}
+        certificate_path=certificate_path,
+        password=cert_secret
     )
 
-    token = provider.acquire_token()
-    assert (token['token_type'] == "Bearer")
-    assert (token['expires_in'] == 3599)
-    assert (len(token['access_token']) > 0)
+    saas = SaaSAPI(cred)
+    subscription_found = False
+    subscriptions = saas.fulfillment_operations.list_subscriptions()
+
+    for sub in subscriptions:
+        if sub.saas_subscription_status == SubscriptionStatusEnum.SUBSCRIBED:
+            subscription_found = True
+
+    assert subscription_found
+
 
